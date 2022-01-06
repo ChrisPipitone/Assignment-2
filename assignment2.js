263255604e37c4d8c6f1afcb572453a803e153c5
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-05 19:27:42 -0500

    took break now finished up to every

--- assignment2.js
+++ assignment2.js
@@ -1,11 +1,10 @@
 //Chris Pipitone Assignment 2
 
 //array used for testing in each assigned function
-const arr = [1, 2 ,3, 4, 5];
+const arr = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10];
 
 
 // === forEach ===
-
 function myEach(myCallBack, arr)
 {
     //Error Handling
@@ -27,7 +26,6 @@ myEach(myCallBack1, arr);
 
 
 // === Map ===
-
 function myMap(myCallBack, arr)
 {
     //Error Handling
@@ -53,7 +51,6 @@ console.log( myMap(myCallBack2, arr) );
 
 
 // === Filter ===
-
 function myFilter(myCallBack, arr)
 {
     //Error Handling
@@ -78,4 +75,59 @@ let myCallBack3 = element => element > 3; //driver function
 console.log( "======== myFilter ========");
 console.log( myFilter(myCallBack3, arr) );
 
-// === Some aka Any ===
+
+// === Some aka Any === 
+function mySome(myCallBack, arr)
+{
+    //Error Handling
+    if (typeof myCallBack !== 'function')
+        throw new TypeError(myCallBack + ' is not a function');
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    for( let i = 0; i < arr.length; i++)
+    {
+        //Some/any returns a boolean based on whether any element results from the callback function
+        if(myCallBack(arr[i])) 
+            return true;
+    }
+    return false;
+}
+let myCallBack4 = element => element % 2 === 0; //driver function
+console.log( "======== mySome ========");
+console.log( mySome(myCallBack4, arr) );
+
+
+// === Every ===
+function myEvery(myCallBack, arr) 
+{
+     //Error Handling
+     if (typeof myCallBack !== 'function')
+        throw new TypeError(myCallBack + ' is not a function');
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    let result = false;
+
+    for( let i = 0; i < arr.length; i++)
+    {  
+         //Every returns a boolean based on if every element passed the test provided by the callback funciton
+        if(myCallBack(arr[i])) 
+            result = true;
+        else
+            return false;
+    }
+    return result;
+}
+let myCallBack5 = element => element < 5 ; //driver function
+console.log( "======== mySome ========");
+console.log( myEvery(myCallBack5, arr) );
+
+
+// === Reduce ===
+// === Includes ===
+// === IndexOf ===
+// === Push ===
+// === LastIndexOf ===
+// === ObjectKeys ===
+// === ObjectValues ===
