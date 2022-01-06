99133c6fddb030c42904dc17d8e1e33d43ef20ee
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-05 20:24:34 -0500

    3 left

--- assignment2.js
+++ assignment2.js
@@ -1,9 +1,9 @@
 //Chris Pipitone Assignment 2
 
 //array used for testing in each assigned function
-const arr = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 2 ,3, 4];
 
-
+//, 5, 6, 7, 8, 9, 10
 // === forEach ===
 function myEach(myCallBack, arr)
 {
@@ -120,14 +120,104 @@ function myEvery(myCallBack, arr)
     return result;
 }
 let myCallBack5 = element => element < 5 ; //driver function
-console.log( "======== mySome ========");
+console.log( "======== myEvery ========");
 console.log( myEvery(myCallBack5, arr) );
 
 
 // === Reduce ===
+function myReduce(myCallBack, arr, initial)
+{
+     //Error Handling
+     if (typeof myCallBack !== 'function')
+        throw new TypeError(myCallBack + ' is not a function');
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    //check for provided initial value
+    let i = 0;
+    if(initial === undefined || initial === null) 
+    {
+        initial = arr[0];
+        i = 1;
+    }
+    //else initial is provided , so we keep i = 0, it addresses the first element
+    //so it starts with initial and element [0]
+    
+        
+    //initializer
+    let curr = myCallBack(initial, arr[i]);
+
+    //a first call was done already need to do next index whether [0] or [1] 
+    for( i += 1 ; i < arr.length; i++)
+    {  
+         //Reduce returns a return value from the calculation on the preceding element. 
+         //The final result of running the reducer across all elements of the array is a single value.
+        curr =  myCallBack(curr, arr[i]);
+    }
+    return curr;
+}
+
+let myCallBack6 = (previousValue, currentValue) => previousValue + currentValue;
+console.log( "======== myReduce ========");
+console.log( myReduce(myCallBack6, arr, 5) );
+
+console.log( "======== myReduce with out initializer ========");
+console.log( myReduce(myCallBack6, arr) );
+
+
 // === Includes ===
+function myIncludes (arr, target)
+{
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    //Includes returns true if the target is an element of the array else returns false
+    for( let i = 0; i < arr.length; i++)
+    {  
+        if(arr[i] === target)
+            return true;
+    }
+    return false;
+}
+
+console.log( "======== myIncludes ========");
+console.log( myIncludes(arr, 10000) );
+
 // === IndexOf ===
+function myIndexOf(arr, target)
+{
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    for( let i = 0; i < arr.length; i++)
+    {  
+        //returns the index of target if it is an element of the array returns -1 if not
+        if(arr[i] === target)
+            return i;
+    }
+    return (-1);
+}
+console.log( "======== myIndexOf ========");
+console.log( myIndexOf(arr, 300) );
+
 // === Push ===
+function myPush(arr, element)
+{
+    if(!Array.isArray(arr))
+        throw new TypeError( arr + 'is not an array');
+
+    //puts element at the end of the array and returns the arrays new length
+    i = arr.length;
+    arr[i] = element;
+
+    return arr.length;
+}
+console.log( "======== myPush ========");
+console.log("arr length:: " + arr.length);
+console.log(arr);
+console.log( "arr length:: " + myPush(arr, 300) );
+console.log(arr);
+
 // === LastIndexOf ===
 // === ObjectKeys ===
 // === ObjectValues ===
